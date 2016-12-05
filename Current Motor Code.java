bcb14c972c910ac25477da28ba857d491cec57cc
Henry <Pvrpl3Afr0@users.noreply.github.com>
2016-12-05 08:02:24 -0500

    Actual Java File for motor code W/ WIP Turning
    To get our robot to turn we need to make the X axis of the right stick power the motors in a clockwise fashion (I think) (Thanks haze)

---
+++ Current Motor Code.java
@@ -0,0 +1,41 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+/**
+ * Created by admin on 11/29/16.
+ */
+@TeleOp(name = "teleop", group = "hi")
+public class cs extends LinearOpMode{
+    DcMotor rightFront;
+    DcMotor leftFront;
+    DcMotor rightBack;
+    DcMotor leftBack;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        rightFront = hardwareMap.dcMotor.get("rf");
+        leftFront = hardwareMap.dcMotor.get("lf");
+        rightBack = hardwareMap.dcMotor.get("rb");
+        leftBack = hardwareMap.dcMotor.get("lb");
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            rightBack.setPower(gamepad1.left_stick_y);
+            leftBack.setPower(-gamepad1.left_stick_y);
+            rightFront.setPower(gamepad1.left_stick_y);
+            leftFront.setPower(-gamepad1.left_stick_y);
+
+            leftBack.setPower(gamepad1.right_stick_x);
+            rightBack.setPower(-gamepad1.right_stick_x);
+            
+
+
+
+        }
+
+    }
+}
